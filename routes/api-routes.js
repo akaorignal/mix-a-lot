585bb040a6495eea2267803f83ab7dd53589f0f2
perltech <noah.perlmutter@gmail.com>
2017-11-04 11:17:07 -0600

    add api sequelize routes to post, get single id, and destroy

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,41 +2,71 @@ let db = require('../models');
 
 module.exports = function(app) {
     /* Liquors */
-    app.get('mix-a-lot/api/liquors', function(req, res) {
+    app.get('/api/liquors', function(req, res) {
         db.Liquor.findAll({})
         .then(function(dbLiquor) {
             res.json(dbLiquor);
         });
     });
 
-  app.post("mix-a-lot/api/liquors", function(req, res) {
-    // console.log(req.body);
-    db.Liquor.create({
-        liquor_name: DataTypes.STRING,
-        liquor_picture: DataTypes.STRING,
-        spirits: DataTypes.STRING,
-        aging: DataTypes.INTEGER,
-        label: DataTypes.STRING,
-        country: DataTypes.STRING,
-        city_of_origin: DataTypes.STRING,
-        proof: DataTypes.INTEGER
-    }).then(function(dbTodo) {
-      res.json(dbTodo);
+    // Search for one liquor
+    app.get("/api/liquors/:id", function(req, res) {
+        db.Liquor.findOne({
+            where: {
+            id: req.params.id
+            }
+        }).then(function(dbLiquor) {
+            res.json(dbLiquor);
+        });
     });
-  });
 
+    app.post("/api/liquors", function(req, res) {
+    // console.log(req.body);
+        db.Liquor.create({
+            liquor_name: DataTypes.STRING,
+            liquor_picture: DataTypes.STRING,
+            spirits: DataTypes.STRING,
+            aging: DataTypes.INTEGER,
+            label: DataTypes.STRING,
+            country: DataTypes.STRING,
+            city_of_origin: DataTypes.STRING,
+            proof: DataTypes.INTEGER
+        }).then(function(dbLiquor) {
+            res.json(dbLiquor);
+        });
+    });
 
+    app.delete("/api/liquors/:id", function(req, res) {
+        db.Liquor.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbLiquor) {
+            res.json(dbLiquor);
+        });
+    });
 
 
     /* Mixed Drinks */
-    app.get('mix-a-lot/api/mixed-drinks', function(req, res) {
+    app.get('/api/mixed-drinks', function(req, res) {
         db.mixedDrinks.findAll({})
         .then(function(dbMixedDrinks) {
             res.json(dbMixedDrinks);
         });
     });
 
-    app.post("mix-a-lot/api/mixed-drinks", function(req, res) {
+    // Search for one mixed
+    app.get("/api/mixed-drinks/:id", function(req, res) {
+        db.mixedDrinks.findOne({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbMixedDrinks) {
+            res.json(dbMixedDrinks);
+        });
+    });
+
+    app.post("/api/mixed-drinks", function(req, res) {
         // console.log(req.body);
         db.mixedDrinks.create({
             mixed_name: DataTypes.STRING,
@@ -44,9 +74,19 @@ module.exports = function(app) {
             country: DataTypes.STRING,
             city_of_origin: DataTypes.STRING,
             proof: DataTypes.INTEGER
-        }).then(function(dbTodo) {
-          res.json(dbTodo);
+        }).then(function(dbMixedDrinks) {
+            res.json(dbMixedDrinks);
         });
-      });    
+    });
+      
+    app.delete("/api/mixeed-drinks/:id", function(req, res) {
+        db.Liquor.destroy({
+            where: {
+                id: req.params.id
+            }
+        }).then(function(dbLiquor) {
+            res.json(dbLiquor);
+        });
+    });
 
 }; // end of module
