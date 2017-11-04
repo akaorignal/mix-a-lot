d0d7c2a50dbbc3cae34ee215de77cbd352a0abe9
perltech <noah.perlmutter@gmail.com>
2017-11-04 10:55:50 -0600

    add post routes to api

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,6 +9,25 @@ module.exports = function(app) {
         });
     });
 
+  app.post("mix-a-lot/api/liquors", function(req, res) {
+    // console.log(req.body);
+    db.Liquor.create({
+        liquor_name: DataTypes.STRING,
+        liquor_picture: DataTypes.STRING,
+        spirits: DataTypes.STRING,
+        aging: DataTypes.INTEGER,
+        label: DataTypes.STRING,
+        country: DataTypes.STRING,
+        city_of_origin: DataTypes.STRING,
+        proof: DataTypes.INTEGER
+    }).then(function(dbTodo) {
+      res.json(dbTodo);
+    });
+  });
+
+
+
+
     /* Mixed Drinks */
     app.get('mix-a-lot/api/mixed-drinks', function(req, res) {
         db.mixedDrinks.findAll({})
@@ -16,4 +35,18 @@ module.exports = function(app) {
             res.json(dbMixedDrinks);
         });
     });
-};
+
+    app.post("mix-a-lot/api/mixed-drinks", function(req, res) {
+        // console.log(req.body);
+        db.mixedDrinks.create({
+            mixed_name: DataTypes.STRING,
+            mixed_picture: DataTypes.STRING,
+            country: DataTypes.STRING,
+            city_of_origin: DataTypes.STRING,
+            proof: DataTypes.INTEGER
+        }).then(function(dbTodo) {
+          res.json(dbTodo);
+        });
+      });    
+
+}; // end of module
