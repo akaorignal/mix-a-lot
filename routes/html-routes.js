c0bed7799278d59f6d6065e279f803572ace0358
perltech <noah.perlmutter@gmail.com>
2017-11-04 13:19:06 -0600

    individual drink id

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,17 +4,40 @@ var db = require('../models');
 module.exports = function(app) {
 
     app.get('/', function(req, res) {
-        //res.sendFile(path.join(__dirname, '../view/index.handlebars'));
-        res.render("index", { home: homeData });
+        res.render('index');
     });
-    // Replace above with
-    //res.render(index, 'handlebars')
 
-    app.get('/liquors', function(req, res) {
+    /* Liquors */
+    app.get('/liquors/all', function(req, res) {
         db.Liquor.findAll({})
         .then(function(dbLiquor) {
             res.render('liquor-list', {liquor: dbLiquor});
         });
     });
 
-};
+    app.get('/liquors/:id', function(req,res) {
+        db.Liquor.findOne({
+            where: {
+            id: req.body.id
+            }
+        });
+    });
+
+
+    /* Mixed Drinks */
+    app.get('/mixed-drinks/all', function(req, res) {
+        db.mixedDrinks.findAll({})
+        .then(function(dbMixedDrinks) {
+            res.render('mixed-drink-list', {mixedDrinks: dbMixedDrinks });
+        });
+    });
+
+    app.get('/mixed-drinks/:id', function(req,res) {
+        db.mixedDrinks.findOne({
+            where: {
+            id: req.body.id
+            }
+        });
+    });
+    
+}; // end of exports
