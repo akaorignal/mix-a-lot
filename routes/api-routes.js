bfaa1b48e179a65b66a766848ee97eed2e320858
perltech <noah.perlmutter@gmail.com>
2017-11-04 10:26:46 -0600

    adds liquor constructor and api route

---
+++ routes/api-routes.js
@@ -0,0 +1,10 @@
+let db = require('../models');
+
+module.exports function(app) {
+    app.get('mix-a-lot/liquors', function(req, res) {
+        db.Liquor.findAll({})
+        .then(function(dbLiquor) {
+            res.json(dbLiquor);
+        });
+    });
+}
