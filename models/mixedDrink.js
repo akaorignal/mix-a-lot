89a73c95e56ef881ac79c147107e92c45e08bdd2
perltech <noah.perlmutter@gmail.com>
2017-11-04 10:37:47 -0600

    add mixed drinks contructor and api route

--- models/mixedDrink.js
+++ models/mixedDrink.js
@@ -1,4 +1,12 @@
-let express = require('express');
+// Constructor for Mixed Drinks in Sequelize
 
-let router = require(express).Router();
+module.exports = function(sequelize, DataTypes) {
+    var mixedDrinks = sequelize.define("Mixed Drinks", {
+        mixed_name: DataTypes.STRING,
+        mixed_picture: DataTypes.STRING,
+        country: DataTypes.STRING,
+        city_of_origin: DataTypes.STRING,
+        proof: DataTypes.INTEGER
+    });
 
+};
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,19 @@
 let db = require('../models');
 
 module.exports = function(app) {
-    app.get('mix-a-lot/liquors', function(req, res) {
+    /* Liquors */
+    app.get('mix-a-lot/api/liquors', function(req, res) {
         db.Liquor.findAll({})
         .then(function(dbLiquor) {
             res.json(dbLiquor);
         });
     });
-}
+
+    /* Mixed Drinks */
+    app.get('mix-a-lot/api/mixed-drinks', function(req, res) {
+        db.mixedDrinks.findAll({})
+        .then(function(dbMixedDrinks) {
+            res.json(dbMixedDrinks);
+        });
+    });
+};
