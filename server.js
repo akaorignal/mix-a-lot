98e437b942dddac24183078df4751f48d08343b5
perltech <noah.perlmutter@gmail.com>
2017-11-06 13:47:49 -0700

    Working on server add, elements to incorporate express

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var router = express.Router();
 
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -22,4 +23,19 @@ var routes = require("./routes/api-routes");
 
 app.use("/", routes);
 
-app.listen(port);
+var db = require("./models");
+
+const apiRoutes = require('./routes/api-routes');
+app.use('/api', apiRoutes);
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
+
+router.get('/', function(req, res) {
+    res.json({ title: 'Express' });
+});
+
+module.exports = router;
