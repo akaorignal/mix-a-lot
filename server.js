1ae769eb27a34537d2ca48e9a8498ad08155a293
perltech <noah.perlmutter@gmail.com>
2017-11-07 17:43:37 -0700

    refactor server

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var router = express.Router();
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const app = express();
+const router = express.Router();
 
-var PORT = process.env.PORT || 3000;
+const routes = require("./routes/api-routes");
+const apiRoutes = require('./routes/api-routes');
+const db = require("./models");
 
-var app = express();
+const PORT = process.env.PORT || 3000;
 
-// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-// Changed where this was targeting from "../mix-a-lot/routes/api-routes.js" -- Noah
-var routes = require("./routes/api-routes");
-
 app.use("/", routes);
-
-var db = require("./models");
-
-const apiRoutes = require('./routes/api-routes');
 app.use('/api', apiRoutes);
 
 db.sequelize.sync().then(function() {
