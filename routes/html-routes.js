7f4d96224a3745b86c84713d32a9acce57b5ba3c
perltech <noah.perlmutter@gmail.com>
2017-11-07 19:13:16 -0700

    show liquor block view

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
     app.get('/liquors/all', function(req, res) {
         db.Liquor.findAll({})
         .then(function(dbLiquor) {
-            res.render('liquor-list', {liquor: dbLiquor});
+            res.render('partials/liquor/liquor-block', {liquor: dbLiquor});
         });
     });
 
