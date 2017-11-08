567aa3b2ea47c69342bb410a4840e7f11aae6770
Nathan <natwave2@gmail.com>
2017-11-07 20:41:51 -0700

    commit

---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO liquor (liquor_name, spirits) VALUES ('Smirnoff', 'vodka');
+
+INSERT INTO liquor (liquor_name, spirits) VALUES ('Seamans', 'Gin');
+
+INSERT INTO liquor (liquor_name, spirits) VALUES ('Smirnoff', 'vodka');
+
+INSERT INTO liquor (liquor_name, spirits) VALUES ('Seamans', 'Gin');
+
+
+INSERT INTO liquor (liquor_name, spirits) VALUES ('Smirnoff', 'vodka');
+
+INSERT INTO liquor (liquor_name, spirits) VALUES ('Seamans', 'Gin');
+
+
+INSERT INTO liquor (liquor_name, spirits) VALUES ('Smirnoff', 'vodka');
+
+
+
+INSERT INTO mixed_drinks (mixed_name, country, city_of_origin) VALUES ('Mohito', 'Spain', 'Bacelona');
+
+INSERT INTO mixed_drinks (mixed_name, country, city_of_origin) VALUES ('Mohito', 'Spain', 'Bacelona');
+
+INSERT INTO mixed_drinks (mixed_name, country, city_of_origin) VALUES ('Mohito', 'Spain', 'Bacelona');
+
+INSERT INTO mixed_drinks (mixed_name, country, city_of_origin) VALUES ('Mohito', 'Spain', 'Bacelona');
+
+
+INSERT INTO mixed_drinks (mixed_name, country, city_of_origin) VALUES ('Mohito', 'Spain', 'Bacelona');
+
+INSERT INTO mixed_drinks (mixed_name, country, city_of_origin) VALUES ('Mohito', 'Spain', 'Bacelona');
+
+
+INSERT INTO mixed_drinks (mixed_name, country, city_of_origin) VALUES ('Mohito', 'Spain', 'Bacelona');
