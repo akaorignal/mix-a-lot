5c7af7232978232ea06962a07cd609f35a57a51c
akaorignal <torir.adams@gmail.com>
2017-11-08 19:50:34 -0800

    csstori

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(express.static(__dirname + '/public'));
 
 //app.use("/", routes);
 // app.use('/api', apiRoutes);
