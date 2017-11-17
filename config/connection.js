13e8422a415a768ba6637f7c3ce2fad40ff21d7c
perltech <noah.perlmutter@gmail.com>
2017-11-16 17:13:24 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,4 @@
 var Sequelize = require("sequelize");
-// var config = require('./config/config.json');
-
-// var sequelize = new Sequelize("all_alcohol_db", "root", ".1.got.d@.p0w3r.", {
-//     host: "127.0.0.1",
-//     dialect: "mysql",
-//     pool: {
-//         max: 5,
-//         min: 0,
-//         idle: 1000
-
-//     }
-// });
 
 if (process.env.JAWSDB_URL) {
 
@@ -18,7 +6,7 @@ if (process.env.JAWSDB_URL) {
 } else {
 
 
-    var sequelize = new Sequelize("all_alcohol_db", "root", "Ak48126969!", {
+    var sequelize = new Sequelize("all_alcohol_db", "root", null, {
         host: "127.0.0.1",
         dialect: "mysql",
         pool: {
